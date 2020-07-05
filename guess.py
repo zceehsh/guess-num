8fa591fa873bbdb2438ace86f6680fad04763828
Haonan Shen <zceehsh@ucl.ac.uk>
2020-07-05 09:59:24 +0800

    add count

--- guess.py
+++ guess.py
@@ -5,13 +5,17 @@
 
 import random
 r = random.randint(1,100)
+count = 0 # 计数
 while True:
+	count += 1 # count = count + 1
 	num = input('请输入1至100间的整数： ')
 	num = int(num)
 	if num == r:
 		print('终于猜对了！')
+		print('一共用了', count, '次猜对！')
 		break
 	elif num < r:
 		print('您猜的太小了。')
 	else:
 		print('您猜的太大了。')
+	print('这是第', count, '次猜测')
